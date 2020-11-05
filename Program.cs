0496d69aae81add81c9f42e96a30787de3054882
Paul <71508910+plesieur@users.noreply.github.com>
2020-11-05 14:53:22 -0500

    Creation
    Hello Class Main Program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Mr. Lesieur!");
+        }
+    }
+}
